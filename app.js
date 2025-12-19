213c163886f7743f3a0b71ce9210336338a4eae7
heroes75 <123456789+odin@users.noreply.github.com>
2025-12-19 22:34:29 +0000

    delete all previous change

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const PORT = 8080
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+
 app.use(express.urlencoded({extended: true}))
 app.use(indexRouter)
 app.use(newRouter)
 app.use(messageRouter)
+
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`)
 })
